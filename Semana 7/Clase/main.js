bb903979967d980ae222581ae2050b7ab1dc8305
IgnaDev <arcortoon@gmail.com>
2024-04-07 12:31:11 -0300

    Agregado primera parte de ejercicios

---
+++ Semana 7/Clase/main.js
@@ -0,0 +1,10 @@
+// Vamos a agregarle un H1 al contenedor mayor y agreguemos un titulo
+
+
+
+// vamos a crear una funcion que tome un nodo y que le agregue una clase
+
+
+// ahora una que podamos agregar una clase a varios nodos que tomamos de una clase, cuidado con el htmlCollection Array.from(collection)
+
+
