84c03d93d40c14cb6584e89a010adc36ced3a4d8
IgnaDev <arcortoon@gmail.com>
2024-04-07 18:46:55 -0300

    Todo listo

---
+++ Semana 7/Clase/mainEvento.js
@@ -0,0 +1,21 @@
+//mousedown/mouseup: Crea un botón en tu HTML. Cuando el usuario haga clic en él (mousedown) y luego suelte el botón (mouseup), cambia el color de fondo del botón.
+ 
+//mouseover/mouseout: Agrega un elemento en tu página. Cuando el usuario mueva el ratón sobre él (mouseover), muestra un mensaje como “¡Estás encima de la imagen!” y cuando el ratón salga del elemento (mouseout), muestra “¡El ratón se fue!”.
+
+//mousemove: Crea un div en tu página. Muestra las coordenadas del ratón dentro del div mientras el usuario mueve el ratón sobre él.
+
+//click: Agrega un botón y un párrafo en tu HTML. Cuando el usuario haga clic en el botón, cambia el texto del párrafo.
+
+
+
+//keydown: Crea un input de texto. Cuando el usuario presione una tecla, muestra la tecla presionada en otro elemento (por ejemplo, un párrafo).
+
+//keyup: Similar al anterior, pero muestra la tecla cuando el usuario suelte la tecla.
+
+//change: Agrega un input de tipo “checkbox”. Cuando el usuario marque o desmarque la casilla, muestra un mensaje como “¡Checkbox seleccionado!” o “¡Checkbox deseleccionado!”.
+
+//input: Crea un input de texto. Muestra el contenido del input en tiempo real mientras el usuario escribe.
+
+//Submit: Crea un formulario con un botón de envío (submit). Cuando el usuario complete el formulario y haga clic en el botón, muestra un mensaje como “Formulario enviado correctamente”.
+
+
