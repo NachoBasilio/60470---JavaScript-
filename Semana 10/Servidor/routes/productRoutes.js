9c3270faa3c515a71f72ee450829ac9065da5245
IgnaDev <arcortoon@gmail.com>
2024-04-26 17:11:28 -0300

    Primera parte del proyecto para el lunes terminado

---
+++ Semana 10/Servidor/routes/productRoutes.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+
+// Obtener todos los productos
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Crear un nuevo producto
+router.post('/', async (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+  });
+  try {
+    const newProduct = await product.save();
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Obtener un producto específico
+router.get('/:id', getProduct, (req, res) => {
+  res.json(res.product);
+});
+
+// Actualizar un producto
+router.put('/:id', getProduct, async (req, res) => {
+  if (req.body.name != null) {
+    res.product.name = req.body.name;
+  }
+  if (req.body.description != null) {
+    res.product.description = req.body.description;
+  }
+  if (req.body.price != null) {
+    res.product.price = req.body.price;
+  }
+  try {
+    const updatedProduct = await res.product.save();
+    res.json(updatedProduct);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Eliminar un producto
+router.delete('/:id', getProduct, async (req, res) => {
+  try {
+    await res.product.remove();
+    res.json({ message: 'Product deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getProduct(req, res, next) {
+  let product;
+  try {
+    product = await Product.findById(req.params.id);
+    if (product == null) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.product = product;
+  next();
+}
+
+module.exports = router;
---
+++ Semana 10/Servidor/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api/products', require('./routes/productRoutes'));
+
+mongoose.connect(process.env.MONGO)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
